16cef5f1fb0d39acc00149b7f9283096646cbb95
Vips43 <vipulgehlot43@gmail.com>
2025-08-15 11:16:43 +0530

    de

--- script.js
+++ script.js
@@ -7,20 +7,27 @@ let phoneNum = document.querySelector('.phone')
 let email = document.querySelector('.mail')
 let pass = document.querySelector('.pass')
 let cPass = document.querySelector('.apass')
+let signUpBtn = document.querySelector('#signupbtn')
 
 
 
-toggleDiv.addEventListener('click',()=>{
-    toggleDiv.classList.toggle("cover")
-    // toggleDivC1.innerHTML = toggleDiv.classList.contains("cover") ? 'Log in' : 'Sign up' 
-    if(toggleDiv.classList.contains("cover")){
-        toggleDivC1.innerHTML = 'Log in'
-        form.style.borderColor = '#3E0703'
-    }else{
-        toggleDivC1.innerHTML = 'Sign up'
-        form.style.borderColor = '#064232'
+
+// toggleDiv.addEventListener('click',()=>{
+//     toggleDiv.classList.toggle("cover")
+//     // toggleDivC1.innerHTML = toggleDiv.classList.contains("cover") ? 'Log in' : 'Sign up' 
+//     if(toggleDiv.classList.contains("cover")){
+//         toggleDivC1.innerHTML = 'Log in'
+//         form.style.borderColor = '#3E0703'
+//     }else{
+//         toggleDivC1.innerHTML = 'Sign up'
+//         form.style.borderColor = '#064232'
         
-    }
+//     }
+// })
+signUpBtn.addEventListener('click',()=>{
+    const inpv = fName.value
+    console.log(inpv);
+    
 })
 
 
