ccc7b25dd212f22436d7ae98a3f8a375fcea5ade
Vips43 <vipulgehlot43@gmail.com>
2025-08-15 12:20:28 +0530

    lo

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 let form = document.getElementById('form')
 let toggleDiv = document.querySelector('.toggle-div')
 let toggleDivC1 = document.querySelector('.toggle-div .c1')
-let fName = document.querySelector('.fname')
-let lName = document.querySelector('.lname')
-let phoneNum = document.querySelector('.phone')
-let email = document.querySelector('.mail')
-let pass = document.querySelector('.pass')
-let cPass = document.querySelector('.apass')
+let fName = document.querySelector('#fname').value
+let lName = document.querySelector('#lname').value
+let phoneNum = document.querySelector('#phone').value
+let email = document.querySelector('#mail').value
+let pass = document.querySelector('#pass').value
+let cPass = document.querySelector('#apass').value
 let signUpBtn = document.querySelector('#signupbtn')
 
 
 
 
-// toggleDiv.addEventListener('click',()=>{
-//     toggleDiv.classList.toggle("cover")
-//     // toggleDivC1.innerHTML = toggleDiv.classList.contains("cover") ? 'Log in' : 'Sign up' 
-//     if(toggleDiv.classList.contains("cover")){
-//         toggleDivC1.innerHTML = 'Log in'
-//         form.style.borderColor = '#3E0703'
-//     }else{
-//         toggleDivC1.innerHTML = 'Sign up'
-//         form.style.borderColor = '#064232'
+toggleDiv.addEventListener('click',()=>{
+    toggleDiv.classList.toggle("cover")
+    // toggleDivC1.innerHTML = toggleDiv.classList.contains("cover") ? 'Log in' : 'Sign up' 
+    if(toggleDiv.classList.contains("cover")){
+        toggleDivC1.innerHTML = 'Log in'
+        form.style.borderColor = '#3E0703'
+    }else{
+        toggleDivC1.innerHTML = 'Sign up'
+        form.style.borderColor = '#064232'
         
-//     }
-// })
+    }
+})
 signUpBtn.addEventListener('click',()=>{
-    const inpv = fName.value
-    console.log(inpv);
+    if(fName == ''||lName==''||phoneNum==''||email==''||pass==''||cPass==''){
+        alert('please enter all fields')
+    }else if(fName<2||lName<2||phoneNum<10||phoneNum>10){
+        alert('enter valid')
+    }
+    else{
+        alert('submittted')
+    }
+    
+    
     
 })
 
